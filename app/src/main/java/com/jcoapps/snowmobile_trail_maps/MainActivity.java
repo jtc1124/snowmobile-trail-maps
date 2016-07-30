cb29a4734b7f9926c3265026d603249dd0c2fe62
Jeremy Colon <jcolon@Jeremys-MacBook-Pro.local>
2016-07-29 21:31:18 -0400

    Added basic map view

--- app/src/main/java/com/jcoapps/snowmobile_trail_maps/MainActivity.java
+++ app/src/main/java/com/jcoapps/snowmobile_trail_maps/MainActivity.java
@@ -3,7 +3,11 @@ package com.jcoapps.snowmobile_trail_maps;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.esri.android.map.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
